c083ee0a5dfa4e136c4a25505b44bda609d97dad
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-15 17:00:09 +0900

    Lesson1Challenge2～Lesson2Challenge10まで完了

--- Java100_questions_cac2nd/src/lesson02/challenge09/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge09/Detective.java
@@ -67,7 +67,13 @@ public class Detective {
 		int weight = Integer.parseInt(weightStr);
 
 		//ここに記述
-
+		if (height > 185 && (weight > 60 && weight < 80)) {
+			System.out.println("犯人：");
+			System.out.println("その方は犯人である可能性がありますね。");
+		} else {
+			System.out.println("犯人：");
+			System.out.println("その方は犯人ではありませんね。");
+		}
 	}
 
 }
