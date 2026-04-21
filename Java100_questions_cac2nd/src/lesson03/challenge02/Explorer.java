ec5657161a5e15fe6fa27dc8d8b0aa08328013df
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-21 15:19:17 +0900

    lesson03 challenge09まで完了

--- Java100_questions_cac2nd/src/lesson03/challenge02/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge02/Explorer.java
@@ -61,9 +61,17 @@ public class Explorer {
 		int targetCans = 50;
 		int inputCans = 0;
 
-
 		//ここにfor文を利用した処理を記述
+		for (int i = 0; i < 3; i++) {
 
+			System.out.println("隊長：");
+			System.out.println("あと" + (3 - i) + "回！\n");
+			System.out.print("袋に何個入れますか？>");
+
+			String str = br.readLine();
+			inputCans += Integer.parseInt(str);
+
+		}
 
 		if (inputCans >= targetCans) {
 			System.out.println("\n隊長：");
